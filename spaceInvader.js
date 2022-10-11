e18250c7b8e491792a28da1fa1250669244e2040
Staf Dierickx <staf.dierickx@student.ehb.be>
2022-10-11 17:34:10 +0200

    created git spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,27 @@
 "use strict";
 
-alert("Draw your space invader here");
+// alert("Draw your space invader here");
+
+//canvas settings
+const canvas = document.getElementById("canvas")
+canvas.width = document.body.clientWidth
+canvas.height = document.body.clientHeight
+
+const context = canvas.getContext("2d") //create 2d context
+
+// context.beginPath() //begin with drawing
+// context.fillRect(0, 0, 500, 500)
+// context.strokeStyle = "black"
+// context.stroke()
+
+context.beginPath()
+context.fillRect(100, 0, 300, 100)
+
+context.fillRect(0, 100, 100, 100)
+context.fillRect(400, 100, 100, 100)
+
+context.fillRect(100, 200, 300, 200)
+context.fillRect(0, 400, 500, 100)
+
+context.fillStyle = "purple"
+context.stroke()
